2b869c6a1eb4ed714560c57f2dd75e2c629be10a
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-05-25 21:04:05 +0200

    create bank account

---
+++ MainAccount/src/mainaccount/MainAccount.java
@@ -0,0 +1,19 @@
+package mainaccount;
+
+public class MainAccount {
+
+    public static void main(String[] args) {
+        AccountPLN acpln = new AccountPLN(); //declaration and instatiation
+        AccountUSD acusd = new AccountUSD();
+        acpln.setTotalAmount(150000);
+        acpln.setMonthLimit(3000);
+        acpln.printTotalAmount();
+        acpln.printMonthLimit();
+        acusd.setTotalAmount(20000);
+        acusd.setMonthLimit(1000);
+        acusd.printTotalAmount();
+        acusd.printMonthLimit();
+    }
+}
+
+
